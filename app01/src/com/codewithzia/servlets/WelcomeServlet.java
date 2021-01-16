329bbb1dbdc39dda105f35043f22107cf7af705f
Mohd Zia Ul Haq <mohdziaulhaq123@gmail.com>
2021-01-17 00:39:21 +0530

    app01 completed

---
+++ app01/src/com/codewithzia/servlets/WelcomeServlet.java
@@ -0,0 +1,24 @@
+package com.codewithzia.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ * Servlet implementation class WelcomeServlet
+ */
+public class WelcomeServlet extends GenericServlet {
+	private static final long serialVersionUID = 1L;
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException{
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<h2>Hello Client</h2>");
+		out.println("<hr></body></html>");
+	}
+}
