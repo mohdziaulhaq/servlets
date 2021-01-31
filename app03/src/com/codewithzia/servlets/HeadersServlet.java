1b1dda10f41f0a63988df238831825ad74540079
Mohd Zia Ul Haq <mohdziaulhaq123@gmail.com>
2021-01-31 17:36:30 +0530

    new commit

--- app03/src/com/codewithzia/servlets/HeadersServlet.java
+++ app03/src/com/codewithzia/servlets/HeadersServlet.java
@@ -1,6 +1,9 @@
 package com.codewithzia.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,21 +13,21 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/headers")
 public class HeadersServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public HeadersServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		Enumeration<String> headerNames = request.getHeaderNames();
+		out.println("<html><body><table border='1'>");
+		out.println("<tr><td colspan=2 align='center'>Request Headers Data</td></tr>");
+		out.println("<tr><th>Header Name</th><th>Header Value</th></tr>");
+		while(headerNames.hasMoreElements()) {
+			String headerName = headerNames.nextElement();
+			String headerValue = request.getHeader(headerName);
+			out.println("<tr>");
+			out.println("<td>"+headerName+"</td>");
+			out.println("<td>"+headerValue+"</td>");
+			out.println("</tr>");
+		}
+		out.println("</table></body></html>");
 	}
-
 }
---
+++ app04/src/com/codewithzia/servlets/LoginServlet.java
@@ -0,0 +1,36 @@
+package com.codewithzia.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		String uname = request.getParameter("uname");
+		String upwd = request.getParameter("upwd");
+		
+		out.println("<html><body><br><br>");
+		out.println("<h2 style='color: red;' align='center'>");
+		if(uname.equalsIgnoreCase("ziya") && upwd.equalsIgnoreCase("ziya2020")) {
+		out.println("User Login SUCCESS");
+		}else {
+			out.println("User Login FAILURE");
+		}
+		out.println("</h2><br><br><h3 style='align='center'>");
+		out.println("<a href='./loginform.html'>");
+		out.println("</html></body></html>");
+		
+		
+	}
+
+}
